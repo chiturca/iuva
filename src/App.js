ad8e414a6911f3a5e0df6825da2f8981c3fe183d
ISMET ACAR <ismtacr@gmail.com>
2023-11-01 21:08:45 +0300

    basic Tailwind configurations have done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h2 className=" text-red-700 font-extrabold">Merhaba</h2>
+      </header>
     </div>
   );
 }
