cbfaf7e7046a82cd42ff0ddcdcd85015cdf3c9dd
Miray <sonmezmiray@gmail.com>
2023-11-01 18:50:27 +0300

    feat: create firebase google auth

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from "react";
-// import { googleProvider } from "../../../firebase/firebase";
+import { signInWithGoogle } from "./Firebase";
 import "./App.css";
 
 function App() {
-  // const handleSignIn = async () => {
-  //   try {
-  //     await googleSignIn();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
   return (
     <div className="App">
       <header className="App-header">
         <div className="border flex flex-col items-center md:w-1/2 m-auto p-14 rounded-3xl shadow-[5px_5px_15px_-1px_rgba(0,0,0,0.3)] z-50">
           <div className="flex justify-center items-center pb-20">
             <button
-              // onClick={handleSignIn}
+              onClick={signInWithGoogle}
               className="py-1 ease-in duration-300  hover:scale-110"
             >
               Sign-in with Google
---
+++ src/Firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { app, auth };
+
+const provider = new GoogleAuthProvider();
+
+export const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log("Google Auth Result:", result);
+    })
+    .catch((error) => {
+      console.log("Google Auth Error:", error);
+    });
+};
