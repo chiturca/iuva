6fcf61782b983191ef7358055ebccb6b55c507c4
Miray <sonmezmiray@gmail.com>
2023-11-02 18:23:47 +0300

    feat: add router to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./pages/Home/index";
+import AnnoucementPool from "./pages/AnnouncementPool/index";
+import CreateAnnouncement from "./pages/CreateAnnouncement/index";
+import Profile from "./pages/Profile/index";
+import MyAnnouncements from "./pages/MyAnnouncements/index";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/annoucementpool" element={<AnnoucementPool />} />
+        <Route path="/createannouncement" element={<CreateAnnouncement />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/myannouncements" element={<MyAnnouncements />} />
+      </Routes>
       <Footer />
     </>
   );
