663195c57a0915faf1fc3aae14c486f34ae0ec78
Miray <sonmezmiray@gmail.com>
2023-11-02 12:19:42 +0300

    build: npm i react-router-dom

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/MyAnnouncements/index.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MyAnnouncements() {
+  return <div>MyAnnouncements</div>;
+}
